47e3fec356ac902b4a19c9aaa37f1b18a41a950d
NatHuSan <116602336+NatHuSan21@users.noreply.github.com>
2024-03-24 21:59:25 -0600

    Delete ST2 directory

--- ST2/Prueba39/utils/data.py
+++
@@ -1,5 +0,0 @@
-class Data:
-    def __init__(self, img, psnr=None, time=None):
-        self.img = img
-        self.psnr = psnr
-        self.time = time
--- ST2/run.sh
+++
@@ -1,8 +0,0 @@
-search_dir="$(pwd)"
-for entry in $(ls $search_dir)
-do
-	echo "Adding $entry"
-	git add $entry
-	git commit -m "Add $entry"
-	git push
-done
